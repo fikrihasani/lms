170fc9d6e9fce3f9b5ece2f33f9e920b02827244
M Fikri Hasani <mfikrihasani@gmail.com>
2022-04-13 16:32:05 +0700

    revise answer and kelas table

---
+++ database/migrations/2022_04_13_090919_remove_questions_id_from_answers.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            $table->dropForeign(['questions_id']);
+            $table->dropColumn('questions_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            $table->foreignId('questions_id')->constrained();
+        });
+    }
+};
