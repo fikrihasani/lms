0e792c37bf1581f4764d14e46e689f8f4050516d
M Fikri Hasani <mfikrihasani@gmail.com>
2022-05-23 13:22:05 +0700

    information page

--- resources/views/informasi.blade.php
+++ resources/views/informasi.blade.php
@@ -4,16 +4,17 @@
     {{ "Sistem Informasi Evaluasi Pembelajaran" }}
 @endsection
 @section('usersection')
+<div class="container" style="padding-top:20px; padding-bottom: 20px">
     <div class="row">
-        <div class="container">
-            <div class="col-lg-6"></div>
-            <div class="col-lg-6"></div>
+        <div class="col-lg-6" style="font-size:30pt; text-align: center"><i class="bi bi-info-circle"></i></div>
+        <div class="col-lg-6">
+            <p>Sistem Evaluasi Pembelajaran adalah sistem informasi berbasis web yang membantu guru untuk melihat pemahaman siswa terhadap materi pembelajaran</p>
         </div>
     </div>
     <div class="row">
-        <div class="container">
-            <div class="col-lg-6"></div>
-            <div class="col-lg-6"></div>
+        <div class="col-lg-6" style="font-size:30pt; text-align:center"><i class="bi bi-people-fill"></i></div>
+        <div class="col-lg-6">
+            <p>Pengguna yang dapat menggunakan sistem informasi ini adalah siswa sekolah dasar dan guru</p>
         </div>
     </div>
     <div class="row" id="FAQ">
@@ -23,4 +24,5 @@
             </ul>
         </div>
     </div>
+</div>
 @endsection
